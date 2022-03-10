2068539bcdf321119eb7c7831d2d0be03094bb8c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-10 10:16:49 -0600

    simple inheritance example

---
+++ oopInJava/inheritance/simpleInheritance.java
@@ -0,0 +1,21 @@
+class Employee{
+    float salary = 40000;
+}
+
+class simpleInheritance extends Employee {
+    float bonus = 10000;
+
+    public static void main(String args []){
+        simpleInheritance p = new simpleInheritance();
+        System.out.println("Programmer salary is " + p.salary);
+        System.out.println("Programmer bonus is  " + p.bonus);
+    }
+}
+
+// side note:
+// in order to call main from second class with commands:
+// javac simpleInheritance.java => compile and creates class file
+// java simpleInheritance 
+//! class with main method needs to be named same as file
+
+
