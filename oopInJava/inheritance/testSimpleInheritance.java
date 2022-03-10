6d83f938ff3599b8e018d85d34b10a812ecb4812
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-10 10:23:07 -0600

    add test simple inheritance

---
+++ oopInJava/inheritance/testSimpleInheritance.java
@@ -0,0 +1,20 @@
+class Animal {
+    void eat(){
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark(){
+        System.out.println("barking...");
+    }
+}
+
+class testSimpleInheritance{
+
+    public static void main(String args []){
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
